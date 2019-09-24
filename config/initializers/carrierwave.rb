faabfe5231cfe9f5b087a135fcf257467e5e0bd1
budstokes16 <budstokes16@gmail.com>
2019-09-24 03:11:19 +0000

    Heroku Fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,13 @@
-# config/initializers/carrierwave.rb
-
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                      
-  config.fog_public = false
+  config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
+
   config.fog_credentials = {
-    provider:              'AWS',                        
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],        
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],        
+    :provider               => 'AWS',                        # required
+    :s3_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
+    :s3_secret_access_key  => ENV['S3_SECRET_KEY'],                     # required
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]      
-  end        
+  config.fog_directory  = ENV['S3_Bucket']                             # required
+  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
